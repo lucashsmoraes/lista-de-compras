ef00ad492237568b0be98e8aec2ea2ceef1afd2f
Lucas Moraes <lucashsmoraes@gmail.com>
2019-01-26 03:22:54 -0200

    adicionando AppBar e configurando o css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import AppBar from "@material-ui/core/AppBar";
 import "./App.css";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#e0f2f1"
+      main: "#2196f3"
     },
     secondary: {
       main: "#b2ebf2"
@@ -18,6 +19,11 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <AppBar position="static" color="primary">
+          <div className="header">
+            <p className="title">Lista de Compras</p>
+          </div>
+        </AppBar>
         <div>
           <h1>Teste</h1>
         </div>
