01c1c01f17d54f7eb042d8a32b8b3567feed5beb
Lucas Moraes <lucashsmoraes@gmail.com>
2019-01-26 02:53:30 -0200

    adicionado material-ui styles e MuiThemeProvider na aplicação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { createMuiTheme } from "@material-ui/core/styles";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import "./App.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#e0f2f1"
+    },
+    secondary: {
+      main: "#b2ebf2"
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <h1>Teste</h1>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
