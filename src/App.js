ca63d673a6acb61945afd14798158c44d83bed72
Lucas Moraes <lucashsmoraes@gmail.com>
2019-01-26 17:36:01 -0200

    criando rota para os componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Header from "./common/Header";
 import Home from "./home";
+import CreateList from "./createList";
 
 const theme = createMuiTheme({
   palette: {
@@ -20,8 +23,15 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Header />
-        <Home />
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/lista" component={CreateList} />
+            </Switch>
+          </div>
+        </Router>
       </MuiThemeProvider>
     );
   }
---
+++ src/createList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CreateList;
