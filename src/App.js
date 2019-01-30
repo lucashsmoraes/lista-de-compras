34f1c4c6e5f6b4d5ffe56541c3f4e111e6e5ad0c
Lucas <lucashsmoraes@gmail.com>
2019-01-30 21:22:35 -0200

    criado a strutura do reducer e store

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { Provider } from "react-redux";
+
 import "./App.css";
 import Header from "./common/Header";
 import Home from "./home";
 import CreateList from "./createList";
+import store from "./store/store";
 
 const theme = createMuiTheme({
   typography: {
-    useNextVariants: true,
+    useNextVariants: true
   },
   palette: {
     primary: {
       main: "#2196f3"
     },
     secondary: {
-      main: '#43a047'
+      main: "#43a047"
     }
   }
 });
@@ -25,17 +28,19 @@ const theme = createMuiTheme({
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <Router>
-          <div>
-            <Header />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/lista" component={CreateList} />
-            </Switch>
-          </div>
-        </Router>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <Router>
+            <div>
+              <Header />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/lista" component={CreateList} />
+              </Switch>
+            </div>
+          </Router>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
